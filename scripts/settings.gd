c9d6cf40c4e342a4b4c02bc43e1d02cd540600ec
Made Redy Wijaya <redy.wijaya24@gmail.com>
2024-10-30 13:40:09 +0700

    Settings Update

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,16 +1,12 @@
 extends Control
 
-# Fungsi untuk slider sound
-func _on_sound_h_slider_value_changed(value: float) -> void:
-	$VBoxContainer/HBoxContainer/SoundPersentase.text = str(value) + "%"
-
 # Fungsi untuk slider music
 func _on_music_h_slider_value_changed(value: float) -> void:
-	$VBoxContainer/HBoxContainer2/MusicPersentase.text = str(value) + "%"
+	$VBoxContainer/HBoxContainer/MusicPersentase.text = str(value) + "%"
 
 # Fungsi untuk slider sfx
 func _on_sfx_h_slider_value_changed(value: float) -> void:
-	$VBoxContainer/HBoxContainer3/SfxPersentase.text = str(value) + "%"
+	$VBoxContainer/HBoxContainer2/SfxPersentase.text = str(value) + "%"
 	
 
 func _on_back_button_pressed() -> void:
