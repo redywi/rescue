3971475b0badacf1cb1320bfe427f69456546a35
Made Redy Wijaya <redy.wijaya24@gmail.com>
2024-10-31 11:07:44 +0700

    Sound test

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -7,8 +7,11 @@ func _on_music_h_slider_value_changed(value: float) -> void:
 # Fungsi untuk slider sfx
 func _on_sfx_h_slider_value_changed(value: float) -> void:
 	$VBoxContainer/HBoxContainer2/SfxPersentase.text = str(value) + "%"
+	var volume_db = -80 + (value / 100) * 80
+	$AudioStreamPlayer.volume_db = volume_db
 	
 
 func _on_back_button_pressed() -> void:
+	$AudioStreamPlayer.play()
 	# Kembali ke scene utamas
-	get_tree().change_scene_to_file("res://scenes/home_screen.tscn")
+	#get_tree().change_scene_to_file("res://scenes/home_screen.tscn")
