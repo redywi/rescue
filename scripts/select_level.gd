0ceda8d30ebeedaf4b1e48eafc4d6c886ef6b40e
Made Redy Wijaya <redy.wijaya24@gmail.com>
2024-11-02 15:00:25 +0700

    Fixing layout

--- scripts/select_level.gd
+++ scripts/select_level.gd
@@ -24,9 +24,13 @@ func _on_button_level_4_pressed() -> void:
 func _on_button_level_5_pressed() -> void:
 	pass # Replace with function body.
 	#get_tree().change_scene("res://scenes/level_5.tscn")
+	
+# Fungsi untuk ketika Level boss dipilih
+func _on_button_level_boss_pressed() -> void:
+	pass # Replace with function body.
+	#get_tree().change_scene("res://scenes/level_boss.tscn")
 
 # Fungsi untuk tombol Back
 func _on_back_button_pressed() -> void:
 	# Kembali ke Main Menu
 	get_tree().change_scene_to_file("res://scenes/home_screen.tscn")
-	
