64208bf94c5404a2f10b95d352d0bb31223aa8e2
parhannn <farhannn58@gmail.com>
2024-11-26 17:51:08 +0700

    Minor update - Add new lvl (lawn 6) - Add new challenge on lvl 6 (protect endangered animal)

--- scripts/select_level.gd
+++ scripts/select_level.gd
@@ -10,33 +10,25 @@ func _on_button_level_1_pressed() -> void:
 	main_music.stop()
 	get_tree().change_scene_to_file("res://scenes/lawn_1.tscn")
 
-# Fungsi untuk ketika Level 2 dipilih
 func _on_button_level_2_pressed() -> void:
 	main_music.stop()
 	get_tree().change_scene_to_file("res://scenes/lawn_2.tscn")
 
-# Fungsi untuk ketika Level 3 dipilih
 func _on_button_level_3_pressed() -> void:
 	main_music.stop()
 	get_tree().change_scene_to_file("res://scenes/lawn_3.tscn")
-
-# Fungsi untuk ketika Level 4 dipilih
+	
 func _on_button_level_4_pressed() -> void:
 	main_music.stop()
 	get_tree().change_scene_to_file("res://scenes/lawn_4.tscn")
 
-# Fungsi untuk ketika Level 5 dipilih
 func _on_button_level_5_pressed() -> void:
 	main_music.stop()
 	get_tree().change_scene_to_file("res://scenes/lawn_5.tscn")
-	
-# Fungsi untuk ketika Level boss dipilih
-func _on_button_level_boss_pressed() -> void:
-	pass # Replace with function body.
-	#main_music.stop()
-	#get_tree().change_scene_to_file("res://scenes/lawn_6.tscn")
 
-# Fungsi untuk tombol Back
+func _on_button_level_6_pressed() -> void:
+	main_music.stop()
+	get_tree().change_scene_to_file("res://scenes/lawn_6.tscn")
+
 func _on_back_button_pressed() -> void:
-	# Kembali ke Main Menu
 	get_tree().change_scene_to_file("res://scenes/home_screen.tscn")
