855c1bce4a04889d50dcab61af2bc308099475a3
parhannn <farhannn58@gmail.com>
2024-12-01 20:23:17 +0700

    Beta Release 1.0.0

--- scripts/home_screen.gd
+++ scripts/home_screen.gd
@@ -12,5 +12,8 @@ func _on_play_button_pressed():
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+
 func _on_exit_button_pressed():
 	get_tree().quit()
--- scripts/splash.gd
+++ scripts/splash.gd
@@ -1,13 +1,9 @@
 extends Node
 
-
 func _ready():
-	# Menunggu selama 3 detik sebelum melanjutkan
 	await get_tree().create_timer(2.5).timeout
 
-	# Pindah ke scene Main Menu
 	_go_to_main_menu()
 
 func _go_to_main_menu():
-	# Ganti scene ke Main Menu
 	get_tree().change_scene_to_file("res://scenes/home_screen.tscn")
