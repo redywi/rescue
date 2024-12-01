09a75b71fae20ff6d5505692f48efe1721e58ebc
parhannn <farhannn58@gmail.com>
2024-12-01 14:28:09 +0700

    Bug fix

--- scripts/pea_blaster.gd
+++ scripts/pea_blaster.gd
@@ -54,8 +54,8 @@ func _on_hitbox_area_entered(area):
 	if area.name == "attack_area_enemy" and not dead:
 		health_bar.value -= 1
 	if area.name == "projectile_area" and not dead:
-		health_bar.value -= 16
+		health_bar.value -= 10
 	if area.name == "projectile_area2" and not dead:
-		health_bar.value -= 14
+		health_bar.value -= 8
 	if area.name == "projectile_area3" and not dead:
-		health_bar.value -= 32
+		health_bar.value -= 20
